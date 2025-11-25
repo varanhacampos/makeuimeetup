82c819a12c4cd32d9fa99e1e1014954dbd35a477
Aranha <141938587+varanhacampos@users.noreply.github.com>
2025-11-24 22:56:50 -0300

    update

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'UIMeetup',
+      formats: ['es'],
+      fileName: () => `index.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
+});
