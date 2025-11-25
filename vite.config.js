b8e7053d1c4d29a86beb54f6e83c3fc5be205778
Aranha <141938587+varanhacampos@users.noreply.github.com>
2025-11-24 22:59:10 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: 'src/index.ts',
-      name: 'UIMeetup',
+      name: 'MakeUIMeetup',
       formats: ['es'],
       fileName: () => `index.js`,
     },
